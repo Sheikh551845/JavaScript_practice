05c9cf08519b7b7c4846569ec180c987233312aa
Sheikh551845 <sheikh551845@gmail.com>
2025-05-24 22:09:42 +0600

    Banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,29 @@ module.exports = {
     },
     plugins: [],
   }
+  // tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        textShadow: {
+          sm: '1px 1px 2px rgba(0,0,0,0.5)',
+          DEFAULT: '2px 2px 4px rgba(0,0,0,0.5)',
+          lg: '3px 3px 6px rgba(0,0,0,0.7)',
+        },
+      },
+    },
+    plugins: [
+      function ({ addUtilities, theme }) {
+        const shadows = theme('textShadow')
+        const newUtilities = Object.entries(shadows).map(([key, value]) => {
+          return {
+            [`.text-shadow${key === 'DEFAULT' ? '' : '-' + key}`]: {
+              textShadow: value,
+            },
+          }
+        })
+        addUtilities(newUtilities)
+      },
+    ],
+  }
+  
