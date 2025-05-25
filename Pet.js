24edd5c0c5f556affc05b5e0ee1c34db83f8f61f
Sheikh551845 <sheikh551845@gmail.com>
2025-05-25 12:15:27 +0600

    Like pet functionality added

--- Pet.js
+++ Pet.js
@@ -1,24 +1,22 @@
-const LoadCategoriesButtons =()=> {
+const LoadCategoriesButtons = () => {
     fetch("https://openapi.programming-hero.com/api/peddy/categories")
-    .then ((res) => res.json())
-    .then ((data)=> ShowCategoriesButton(data.categories))
+        .then((res) => res.json())
+        .then((data) => ShowCategoriesButton(data.categories))
 }
 
-const LoadAllCategories =()=>
-{
+const LoadAllCategories = () => {
     fetch("https://openapi.programming-hero.com/api/peddy/pets")
-    .then ((res) => res.json())
-    .then ((data)=> ShowAllCategories(data.pets))
+        .then((res) => res.json())
+        .then((data) => ShowAllCategories(data.pets))
 }
 
-const ShowCategoriesButton=(data)=>
-{
-    
-   const PetButtonDiv= document.getElementById("allCategories")
-   data.forEach(element => {
-   
-    const CategoryButtons= document.createElement('div')
-    CategoryButtons.innerHTML=`
+const ShowCategoriesButton = (data) => {
+
+    const PetButtonDiv = document.getElementById("allCategories")
+    data.forEach(element => {
+
+        const CategoryButtons = document.createElement('div')
+        CategoryButtons.innerHTML = `
     
     <div class="border-1 border-stone-500 rounded-lg" onclick=CategoryButtonCliked('${element?.category}') >
     <div class="py-3 px-5 lg:py-3 lg:px-10  flex gap-2 justify-center items-center">
@@ -36,52 +34,65 @@ const ShowCategoriesButton=(data)=>
     </div>
     
     `
-    PetButtonDiv.appendChild(CategoryButtons)
-   });
- 
+        PetButtonDiv.appendChild(CategoryButtons)
+    });
+
 
 
 }
 
-const ShowAllCategories=(pets)=>
-{
-   
- const cardContainer=document.getElementById("CardContainer")
-  cardContainer.innerHTML=""
+const ShowAllCategories = (pets) => {
 
-   if(pets?.length==0){
-      const notFound=document.getElementById("NotFound")
-    const singleCard=document.createElement('div')
-      
-        singleCard.innerHTML=NoResult()
+    const cardContainer = document.getElementById("CardContainer")
+    cardContainer.innerHTML = ""
+
+    if (pets?.length == 0) {
+        const notFound = document.getElementById("NotFound")
+        const singleCard = document.createElement('div')
+
+        singleCard.innerHTML = NoResult()
         notFound.appendChild(singleCard)
     }
 
 
- pets?.forEach(element => {
-   
-    const singleCard=document.createElement('div')
-   
-    singleCard.innerHTML=cards(element)
- 
-     cardContainer.appendChild(singleCard)
-    
-   
-    
- })
-   
+    pets?.forEach(element => {
+
+        const singleCard = document.createElement('div')
+
+        singleCard.innerHTML = cards(element)
+
+        cardContainer.appendChild(singleCard)
+
+
+
+    })
+
 }
 
-const LoadAllCategoryById =(category)=>
-{
+const LoadAllCategoryById = (category) => {
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
-    .then ((res) => res.json())
-    .then ((data)=> ShowAllCategories(data.data))
+        .then((res) => res.json())
+        .then((data) => ShowAllCategories(data.data))
 }
 
-const CategoryButtonCliked =(data)=>
-{
- LoadAllCategoryById(data)
+//Pet liked
+const PetLiked = (photo) => {
+  const LikedContainer = document.getElementById("Liked");
+  const LikedPhoto = document.createElement('div');
+
+
+  LikedPhoto.innerHTML = `
+    <div>
+      <img src='${photo}' alt="" class="w-32 h-32 object-cover rounded" />
+    </div>
+  `;
+
+  LikedContainer.appendChild(LikedPhoto); 
+};
+
+
+const CategoryButtonCliked = (data) => {
+    LoadAllCategoryById(data)
 }
 LoadCategoriesButtons()
 LoadAllCategories()
